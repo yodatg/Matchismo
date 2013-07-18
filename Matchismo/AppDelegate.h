b6ccba8d8e969af1df0c08eba1d66f94607db76d
Thomas Grant <tom@Thomass-MacBook-Pro.local>
2013-07-18 20:17:40 +0100

    Initial Commit

---
+++ Matchismo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Matchismo
+//
+//  Created by Thomas Grant on 18/07/2013.
+//  Copyright (c) 2013 Thomas Grant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Matchismo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Matchismo
+//
+//  Created by Thomas Grant on 18/07/2013.
+//  Copyright (c) 2013 Thomas Grant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
